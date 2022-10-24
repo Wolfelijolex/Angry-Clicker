5b97f7da933259ce509b05792427fddd15d72a11
Alexander G <alexgaertner1@hotmail.com>
2022-10-24 10:40:47 +0200

    added angry

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/angryScaled.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Become angry
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://discord.gg/2hzaK9zPxx"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Join the angry!
         </a>
       </header>
     </div>
