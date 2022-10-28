263277a12cadae559ba71dd2cc637bba832dbeb2
Alex G <alexgaertner1@hotmail.com>
2022-10-28 18:06:57 +0200

    Added tailwind and Sass the project

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../../assets/angryScaled.png";
-import "../../styles/pages/App.scss";
+import styles from "../../styles/pages/App.module.scss";
 import MyComponent from "../../components/MyComponent/MyComponent";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
@@ -11,15 +11,15 @@ function App() {
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Become angry</p>
-        <a className="App-link" href="https://discord.gg/2hzaK9zPxx" target="_blank" rel="noopener noreferrer">
+    <div className={styles.App}>
+      <header className={styles.App_header}>
+        <img src={logo} className={styles.App_logo} alt="logo" />
+        <p className="text-3xl font-bold">Become angry</p>
+        <a className={styles.App_link} href="https://discord.gg/2hzaK9zPxx" target="_blank" rel="noopener noreferrer">
           Join the angry!
         </a>
         <MyComponent title="Title of MyComponent" text="Text of MyComponent"></MyComponent>
-        <button onClick={() => dispatch(moneySlice.actions.add(1))}>
+        <button className="text-amber-400 font-sans" onClick={() => dispatch(moneySlice.actions.add(1))}>
           Click me to get Money! you have {money} coins
         </button>
       </header>
