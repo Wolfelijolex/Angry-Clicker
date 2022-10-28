a696ec2c24b40d963e01627bf671bcfdb68d25d3
Alex G <alexgaertner1@hotmail.com>
2022-10-28 18:07:26 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     ".src/pages/**/*.{js,jsx,ts,tsx}",
+    ".src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
