f1b26f704ee6d3a5e824942ebf84ec45e9e3f5f1
Felix s1910238061 <s2210629018@fhooe.at>
2022-11-12 13:38:55 +0100

    button now does something typescriptyyy

--- src/components/CreditsPopupComponent/CreditsPopupComponent.tsx
+++ src/components/CreditsPopupComponent/CreditsPopupComponent.tsx
@@ -2,26 +2,12 @@ import React, { useState } from "react";
 import styles from "styles/components/CreditsPopUp.module.scss";
 import CreditsPopUp from "./CreditsPopUp";
 
-let settings = false;
-
-function creditsButtonClick() {
-  settings = true;
-  alert(settings);
-}
-function settingsButtonClick() {
-  settings = false;
-
-  alert(
-    "Congratulations! You won a free iPhone 12 Pro Max! Please send your address to the following email: angry.advertisment@gmail.com. For shipping we will need your credit card number, expiration date, and security code."
-  );
-}
-
 function creditsPopupComponent() {
   const [openPopup, setOpenPopup] = useState(false);
 
   return (
     <div>
-      <button className={styles.CreditsButton} onClick={creditsButtonClick}>
+      <button className={styles.CreditsButton} onClick={() => setOpenPopup(true)}>
         i
       </button>
       <CreditsPopUp isOpen={openPopup}></CreditsPopUp>
