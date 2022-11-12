a1dc41645e139870577d87333867e82a658bf90a
Felix s1910238061 <s2210629018@fhooe.at>
2022-11-12 15:37:02 +0100

    added a little bit of style

--- src/components/CreditsPopupComponent/CreditsPopUp.tsx
+++ src/components/CreditsPopupComponent/CreditsPopUp.tsx
@@ -11,9 +11,21 @@ const CreditsPopUp = (props: CreditsPopUpProps) => {
   if (!isOpen) {
     return null;
   } else if (isOpen) {
-    return <div className={styles.creditsPopUpBackground} onClick={() => isOpen}>
-      <div className={styles.creditsButtonOverLayMenu}></div>
-    </div>;
+    return (
+      <div className={styles.creditsPopUpBackground} onClick={() => isOpen}>
+        <div className={styles.creditsButtonOverLayMenu}>
+          <div className={styles.TopBar}>
+            <div className={styles.creditsButtonOverLayMenu__header}> Credits </div>
+            <button className={styles.closeButton}>x</button>
+          </div>
+          <div className={styles.creditsButtonOverLayMenu__body}>
+            <div>
+              <p>Hello</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
   return null;
 };
