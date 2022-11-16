e73b85b5ebe677332165261e9708af3c7be27683
JohannaKrennhuber <65074990+JohannaKrennhuber@users.noreply.github.com>
2022-11-16 15:26:35 +0100

    classNames were changed
    classNames of Clicker and PopUp have been changed to fit overall project

--- src/components/PopUpComponent/PopUpComponent.tsx
+++ src/components/PopUpComponent/PopUpComponent.tsx
@@ -11,17 +11,17 @@ function PopUpComponent(props: PopUpComponentProps) {
   const [show, setShow] = React.useState(true);
 
   return (
-    <div className={popUpStyle.popUpAd}>
-      <button className={popUpStyle.App_button} onClick={() => setShow(!show)}>
+    <div className={popUpStyle.PopUpComponent_popUpAd}>
+      <button className={popUpStyle.PopUpComponent_button} onClick={() => setShow(!show)}>
         ✉️😡 X
       </button>
       {show ? (
         <div>
-          <div className={popUpStyle.container}>
-            <h1 className={popUpStyle.title}>{props.title}</h1>
+          <div className={popUpStyle.PopUpComponent_container}>
+            <h1 className={popUpStyle.PopUpComponent_title}>{props.title}</h1>
             <p>Engage the Rage! Be one with the angry!</p>
             <a
-              className={popUpStyle.App_link}
+              className={popUpStyle.PopUpComponent_link}
               href="https://discord.gg/2hzaK9zPxx"
               target="_blank"
               rel="noopener noreferrer"
